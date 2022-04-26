68a4cb1d0c9cb831bd44e233fd94120f8f635003
nasser amini <na3ramini.73@gmail.com>
2022-04-26 12:54:17 +0430

    script completed and updated

--- script/build-framework.sh
+++ script/build-framework.sh
@@ -3,11 +3,13 @@
 rm -rf frameworks
 mkdir frameworks 
 
+cd ..
+
 
 xcodebuild archive \
 -scheme Adtrace \
 -destination "generic/platform=iOS" \
--archivePath ./frameworks/Adtrace-iOS \
+-archivePath ./script/frameworks/Adtrace-iOS \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -15,7 +17,7 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
 -scheme Adtrace \
 -destination "generic/platform=iOS Simulator" \
--archivePath ./frameworks/Adtrace-iOS-Simulator \
+-archivePath ./script/frameworks/Adtrace-iOS-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -24,14 +26,14 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
 -scheme AdtraceSdk \
 -destination "generic/platform=iOS" \
--archivePath ./frameworks/AdtraceSdk-iOS \
+-archivePath ./script/frameworks/AdtraceSdk-iOS \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild archive \
 -scheme AdtraceSdk \
 -destination "generic/platform=iOS Simulator" \
--archivePath ./frameworks/AdtraceSdk-iOS-Simulator \
+-archivePath ./script/frameworks/AdtraceSdk-iOS-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -40,14 +42,14 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
 -scheme AdtraceStatic \
 -destination "generic/platform=iOS" \
--archivePath ./frameworks/AdtraceStatic-iOS \
+-archivePath ./script/frameworks/AdtraceStatic-iOS \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild archive \
 -scheme AdtraceStatic \
 -destination "generic/platform=iOS Simulator" \
--archivePath ./frameworks/AdtraceStatic-iOS-Simulator \
+-archivePath ./script/frameworks/AdtraceStatic-iOS-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -55,14 +57,14 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
 -scheme AdtraceBridge \
 -destination "generic/platform=iOS" \
--archivePath ./frameworks/AdtraceBridge-iOS \
+-archivePath ./script/frameworks/AdtraceBridge-iOS \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild archive \
 -scheme AdtraceBridge \
 -destination "generic/platform=iOS Simulator" \
--archivePath ./frameworks/AdtraceBridge-iOS-Simulator \
+-archivePath ./script/frameworks/AdtraceBridge-iOS-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -70,27 +72,27 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive \
 -scheme AdtraceBridge \
 -destination "generic/platform=iOS" \
--archivePath ./frameworks/AdtraceBridge-iOS \
+-archivePath ./script/frameworks/AdtraceBridge-iOS \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild archive \
 -scheme AdtraceBridge \
 -destination "generic/platform=iOS Simulator" \
--archivePath ./frameworks/AdtraceBridge-iOS-Simulator \
+-archivePath ./script/frameworks/AdtraceBridge-iOS-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 
 
 xcodebuild -create-xcframework \
--framework ./frameworks/AdtraceSdk-iOS-Simulator.xcarchive/Products/Library/Frameworks/AdtraceSdk.framework \
--output ./frameworks/AdtraceSdk-iOS-Simulator.xcframework
+-framework ./script/frameworks/AdtraceSdk-iOS-Simulator.xcarchive/Products/Library/Frameworks/AdtraceSdk.framework \
+-output ./script/frameworks/AdtraceSdk-iOS-Simulator.xcframework
 
 xcodebuild -create-xcframework \
--framework ./frameworks/AdtraceSdk-iOS.xcarchive/Products/Library/Frameworks/AdtraceSdk.framework \
--output ./frameworks/AdtraceSdk-iOS.xcframework
+-framework ./script/frameworks/AdtraceSdk-iOS.xcarchive/Products/Library/Frameworks/AdtraceSdk.framework \
+-output ./script/frameworks/AdtraceSdk-iOS.xcframework
 
 
-
+cd script
 

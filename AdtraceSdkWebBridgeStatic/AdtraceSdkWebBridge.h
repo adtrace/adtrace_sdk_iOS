53972f67e4ab5fe7ad85773712eb98f299083480
Nasser Amini <namini40@gmail.com>
2023-11-25 12:41:04 +0330

    update Web Bridge modules

--- AdtraceSdkWebBridgeStatic/AdtraceSdkWebBridge.h
+++ AdtraceSdkWebBridgeStatic/AdtraceSdkWebBridge.h
@@ -1,16 +1,9 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for AdtraceSdkWebBridge.
-FOUNDATION_EXPORT double AdtraceSdkWebBridgeVersionNumber;
-
-//! Project version string for AdtraceSdkWebBridge.
-FOUNDATION_EXPORT const unsigned char AdtraceSdkWebBridgeVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AdtraceSdkWebBridge/PublicHeader.h>
-
 #import <AdtraceSdkWebBridge/Adtrace.h>
 #import <AdtraceSdkWebBridge/AdtraceBridge.h>
+#import <AdtraceSdkWebBridge/AdtraceBridgeRegister.h>
 #import <AdtraceSdkWebBridge/ADTEvent.h>
 #import <AdtraceSdkWebBridge/ADTThirdPartySharing.h>
 #import <AdtraceSdkWebBridge/ADTConfig.h>

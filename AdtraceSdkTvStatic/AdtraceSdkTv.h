22d01b6bb7dd650c5e5ac1d8040398e7969e8a96
Nasser Amini <namini40@gmail.com>
2023-11-25 12:40:31 +0330

    update tv modules

--- AdtraceSdkTvStatic/AdtraceSdkTv.h
+++ AdtraceSdkTvStatic/AdtraceSdkTv.h
@@ -1,14 +1,6 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for AdtraceSdkTv.
-FOUNDATION_EXPORT double AdtraceSdkTvVersionNumber;
-
-//! Project version string for AdtraceSdkTv.
-FOUNDATION_EXPORT const unsigned char AdtraceSdkTvVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AdtraceSdkTv/PublicHeader.h>
-
 #import <AdtraceSdkTv/Adtrace.h>
 #import <AdtraceSdkTv/ADTEvent.h>
 #import <AdtraceSdkTv/ADTThirdPartySharing.h>

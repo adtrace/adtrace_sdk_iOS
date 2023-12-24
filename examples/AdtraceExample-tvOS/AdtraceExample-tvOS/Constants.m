6bf1b35ce87b5ce91a875c3c573cc862d8eb7142
Nasser Amini <namini40@gmail.com>
2023-12-24 15:20:38 +0330

    refact: clean and remove unneccessary projects

--- examples/AdtraceExample-tvOS/AdtraceExample-tvOS/Constants.m
+++
@@ -1,8 +0,0 @@
-
-#import "Constants.h"
-
-NSString * const kAppToken = @"cn2dajeoy3uu";
-NSString * const kEventToken1 = @"i53rc7";
-NSString * const kEventToken2 = @"drv45k";
-NSString * const kEventToken3 = @"3vgls9";
-NSString * const kEventToken4 = @"8ihwpl";
--- examples/AdtraceExample-tvOS/AdtraceExample-tvOS/ViewControllertvOS.h
+++
@@ -1,6 +0,0 @@
-
-#import <UIKit/UIKit.h>
-
-@interface ViewControllertvOS : UIViewController
-
-@end
